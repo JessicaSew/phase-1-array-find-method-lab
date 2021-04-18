00815a659ca269b5b639bafb94c6f5fa29fa210c
jessicasewell <sewell.jessica95@gmail.com>
2021-04-18 17:56:05 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+function superbowlWin(record) 
+{
+  const recordResult = record.find( record => record.result === "W")
+  if (typeof (recordResult) === "undefined") {
+      return undefined
+  }
+  else {
+    return recordResult.year   
+  }
+
+}
